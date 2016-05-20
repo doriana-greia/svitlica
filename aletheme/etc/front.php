ed8cd992f57deb0844af1f7d89d899a486bbc36d
doriana-greia <doriana-greia@ukr.net>
2016-05-21 01:05:42 +0300

    commit: add css, js

--- aletheme/etc/front.php
+++ aletheme/etc/front.php
@@ -40,17 +40,13 @@ add_action('wp_print_scripts', 'ale_init_js_vars');
 function ale_enqueue_styles() {
 
 	// add general css file
-	wp_register_style( 'aletheme_general_css', THEME_URL . '/css/general.css', array(), ALETHEME_THEME_VERSION, 'all');
-    wp_register_style( 'jquery.jscrollpane', THEME_URL . '/css/jquery.jscrollpane.css', array(), ALETHEME_THEME_VERSION, 'all');
-    wp_register_style( 'jquery.fancybox-1.3.4', THEME_URL . '/css/jquery.fancybox-1.3.4.css', array(), ALETHEME_THEME_VERSION, 'all');
-    wp_enqueue_style('aletheme_general_css');
 
-    if(is_page_template('template-about.php') or is_page_template('template-award.php')){
-        wp_enqueue_style('jquery.jscrollpane');
-    }
-    if(is_page_template('template-press.php')){
-        wp_enqueue_style('jquery.fancybox-1.3.4');
-    }
+    wp_register_style( 'aletheme_general_css', THEME_URL . '/css/general.css', array(), ALETHEME_THEME_VERSION, 'all');
+    wp_register_style( 'aletheme_owl_carousel', THEME_URL . '/js/libs/owl/assets/owl.carousel.css', array(), ALETHEME_THEME_VERSION, 'all');
+    wp_enqueue_style('aletheme_general_css');
+    wp_enqueue_style('aletheme_owl_carousel');
+
+
 }
 add_action( 'wp_enqueue_scripts', 'ale_enqueue_styles' );
 
@@ -84,25 +80,14 @@ function ale_enqueue_scripts() {
 	// add modernizr
 	wp_register_script( 'ale_modernizr', THEME_URL . '/js/libs/modernizr-2.5.3.min.js', array( 'jquery' ), ALETHEME_THEME_VERSION, false );
 
-    wp_register_script( 'ale_modules', THEME_URL . '/js/modules.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
-    wp_register_script( 'ale_scripts', THEME_URL . '/js/scripts.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
+    wp_register_script( 'ale_common', THEME_URL . '/js/common.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
+    wp_register_script( 'ale_libs', THEME_URL . '/js/libs.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
 
 	wp_enqueue_script( 'jquery-form' );
 	wp_enqueue_script( 'ale_modernizr' );
 	wp_enqueue_script( 'html5-shim' );
-
-    wp_register_script( 'jquery.mousewheel', THEME_URL . '/js/libs/jquery.mousewheel.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
-    wp_register_script( 'jquery.fancybox', THEME_URL . '/js/libs/jquery.fancybox-1.3.4.pack.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
-    wp_register_script( 'jquery.jscrollpane.min', THEME_URL . '/js/libs/jquery.jscrollpane.min.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
-    wp_register_script( 'scrollable', THEME_URL . '/js/libs/scrollable.js', array( 'jquery' ), ALETHEME_THEME_VERSION, true );
-
-    wp_enqueue_script( 'jquery.mousewheel' );
-    wp_enqueue_script( 'jquery.fancybox' );
-    wp_enqueue_script( 'jquery.jscrollpane.min' );
-    wp_enqueue_script( 'scrollable' );
-    wp_enqueue_script( 'ale_modules' );
-	wp_enqueue_script( 'ale_scripts' );
-
+	wp_enqueue_script( 'ale_libs' );
+    wp_enqueue_script( 'ale_common' );
 
 }
 add_action( 'wp_enqueue_scripts', 'ale_enqueue_scripts');
